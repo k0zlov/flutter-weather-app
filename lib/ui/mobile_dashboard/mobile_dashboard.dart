aa2d4d19c582e825b7f123dced8657e9f622f6a2
Morro <ivankozlov0624@gmail.com>
2024-02-08 02:07:23 +0100

    Mobile Location Delete

--- lib/ui/mobile_dashboard/mobile_dashboard.dart
+++ lib/ui/mobile_dashboard/mobile_dashboard.dart
@@ -5,6 +5,7 @@ import 'package:weather_app/ui/home/home_state.dart';
 import 'package:weather_app/ui/home/home_view_model.dart';
 import 'package:weather_app/ui/widgets/content_container.dart';
 import 'package:weather_app/ui/widgets/search_field.dart';
+import 'package:weather_app/utils/temperature_converter.dart';
 import 'package:weather_icons/weather_icons.dart';
 
 class MobileDashboardPage extends StatelessWidget {
@@ -15,7 +16,6 @@ class MobileDashboardPage extends StatelessWidget {
     final HomeViewModel viewModel = context.read<HomeViewModel>();
     final HomePageState state = context.select((HomeViewModel viewModel) => viewModel.state);
 
-
     return Padding(
       padding: const EdgeInsets.all(20.0),
       child: Column(
@@ -25,16 +25,31 @@ class MobileDashboardPage extends StatelessWidget {
           Expanded(
             child: ListView(
               children: [
+                state.locations.isEmpty
+                    ? Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+                        child: Center(
+                          child: Text('Add new location', style: Theme.of(context).textTheme.headlineMedium),
+                        ),
+                      )
+                    : const SizedBox(),
                 ...state.locations.map((location) {
                   final WeatherEntity weather = location.currentWeather;
 
                   return Padding(
                     padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 12),
                     child: MobileLocationWidget(
-                      maxTemp: weather.maxTemperature,
-                      minTemp: weather.minTemperature,
+                      id: location.id,
+                      maxTemp: state.isFahrenheit
+                          ? TemperatureConverter.celsiusToFahrenheit(weather.maxTemperature)
+                          : weather.maxTemperature,
+                      minTemp: state.isFahrenheit
+                          ? TemperatureConverter.celsiusToFahrenheit(weather.minTemperature)
+                          : weather.minTemperature,
                       cityName: location.geocoding.city,
-                      temp: weather.temperature,
+                      temp: state.isFahrenheit
+                          ? TemperatureConverter.celsiusToFahrenheit(weather.temperature)
+                          : weather.temperature,
                       description: weather.description,
                       icon: weather.icon,
                       onTap: () {
@@ -60,9 +75,13 @@ class MobileLocationWidget extends StatelessWidget {
     required this.minTemp,
     required this.temp,
     required this.cityName,
-    required this.description, required this.icon, required this.onTap,
+    required this.description,
+    required this.icon,
+    required this.onTap,
+    required this.id,
   });
 
+  final int id;
   final int maxTemp;
   final int minTemp;
   final int temp;
@@ -73,45 +92,68 @@ class MobileLocationWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final HomeViewModel viewModel = context.read<HomeViewModel>();
+
     return GestureDetector(
       onTap: onTap,
-      child: ContentContainer(
-        width: double.infinity,
-        height: 150,
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
+      child: Dismissible(
+        key: Key(id.toString()),
+        direction: DismissDirection.horizontal,
+        background: Container(
+          decoration: BoxDecoration(
+            color: Colors.redAccent,
+            borderRadius: BorderRadius.circular(20),
+          ),
+          padding: const EdgeInsets.all(8),
+          child: const Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Row(
-                    children: [
-                      BoxedIcon(icon, size: 50),
-                      Text(cityName, style: Theme.of(context).textTheme.headlineMedium),
-                    ],
-                  ),
-                  Text('$temp°', style: Theme.of(context).textTheme.headlineLarge),
-                ],
-              ),
-              DefaultTextStyle(
-                style: Theme.of(context).textTheme.headlineSmall!,
-                child: Row(
+              Icon(Icons.delete, size: 50),
+              Icon(Icons.delete, size: 50),
+            ],
+          ),
+        ),
+        onDismissed: (direction) {
+          viewModel.deleteLocation(id: id);
+        },
+        child: ContentContainer(
+          width: double.infinity,
+          height: 150,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Text(description),
                     Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        Text('H:$maxTemp°'),
-                        Text('L:$minTemp°'),
+                        BoxedIcon(icon, size: 50),
+                        Text(cityName, style: Theme.of(context).textTheme.headlineMedium),
                       ],
-                    )
+                    ),
+                    Text('$temp°', style: Theme.of(context).textTheme.headlineLarge),
                   ],
                 ),
-              )
-            ],
+                DefaultTextStyle(
+                  style: Theme.of(context).textTheme.headlineSmall!,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(description),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text('H:$maxTemp°'),
+                          Text('L:$minTemp°'),
+                        ],
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
